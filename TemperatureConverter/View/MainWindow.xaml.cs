349f0a9ca453a116863d825e8bdd24e32959c305
JasperRutten1 <jasper.rutten@student.ucll.be>
2022-05-11 13:26:32 +0200

    celsius-binding

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Data;
 
 namespace View
 {
@@ -29,8 +30,26 @@ namespace View
             var cel = kel - 273.15;
             var far = (cel * 1.8) + 32;
 
-            celsiusBox.Text = "" + cel;
             fahrenheitBox.Text = "" + far;
+        }
+    }
+
+    public class CelsiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var celsius = kelvin - 273.15;
+
+            return celsius.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var celsius = double.Parse((string)value);
+            var kelvin = celsius + 273.15;
+
+            return kelvin;
         }
     }
 }
