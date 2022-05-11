ea94c81c37be83b544ee72ed6e48ff93a563d284
JasperRutten1 <jasper.rutten@student.ucll.be>
2022-05-11 13:31:44 +0200

    celsius-binding 2

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -33,23 +33,4 @@ namespace View
             fahrenheitBox.Text = "" + far;
         }
     }
-
-    public class CelsiusConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            var celsius = kelvin - 273.15;
-
-            return celsius.ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var celsius = double.Parse((string)value);
-            var kelvin = celsius + 273.15;
-
-            return kelvin;
-        }
-    }
 }
