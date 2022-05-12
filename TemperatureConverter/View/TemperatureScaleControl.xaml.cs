80b5bc6ec39a1273e77174bebada3350dd924fae
JasperRutten1 <jasper.rutten@student.ucll.be>
2022-05-12 19:57:10 +0200

    value-0

--- TemperatureConverter/View/TemperatureScaleControl.xaml.cs
+++ TemperatureConverter/View/TemperatureScaleControl.xaml.cs
@@ -37,5 +37,17 @@ namespace View
             DependencyProperty.Register("Header", typeof(string), typeof(TemperatureScaleControl), new PropertyMetadata(""));
 
 
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(double), new PropertyMetadata(0.0));
+
+
     }
 }
